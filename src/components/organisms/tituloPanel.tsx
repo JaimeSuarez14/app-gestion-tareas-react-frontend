12d75f9cf551cdb66826b83577e0dca7a166ccd2
Jaime Suarez <jaisua8@gmail.com>
2026-02-03 22:27:25 -0500

    creacion de los props de la cabecera

--- src/components/organisms/tituloPanel.tsx
+++ src/components/organisms/tituloPanel.tsx
@@ -1,11 +1,20 @@
-const TituloPanel = () => {
+import type { JSX } from "react";
+
+const TituloPanel = ({
+  titulo,
+  iconTypea,
+}: {
+  titulo: string;
+  iconTypea: JSX.Element;
+}) => {
   return (
     <div className="flex flex-col border-b sm:flex-row justify-center sm:justify-between items-center sm:items-start gap-4 sm:gap-2 text-center sm:text-left w-full">
       <div className="flex-1 min-w-0 sm:flex-initial">
-        <h2 className="text-2xl sm:text-3xl font-bold text-foreground mb-2 truncate">
-          Dashboard
+        <h2 className="text-2xl sm:text-3xl font-bold text-foreground mb-2 truncate flex items-center gap-2">
+          {iconTypea }
+          <span>{titulo}</span>
         </h2>
-        <p className="text-sm sm:text-base text-muted-foreground max-w-xs sm:max-w-none mx-auto sm:mx-0">
+        <p className="text-sm sm:text-base text-gray-400 max-w-xs sm:max-w-none mx-auto sm:mx-0">
           Resumen de tu tienda
         </p>
       </div>
--- src/pages/dashboard/home.tsx
+++ src/pages/dashboard/home.tsx
@@ -1,6 +1,8 @@
 import { CartesianGrid, Legend, Line, LineChart, Tooltip, XAxis, YAxis } from "recharts";
 import PanelSuperior from "../../components/organisms/panelSuperior";
 import TituloPanel from "../../components/organisms/tituloPanel";
+import { RxDashboard } from "react-icons/rx";
+
 
 export type Course = {
   id: string;
@@ -80,7 +82,7 @@ const data = [
 export default function Home() {
   return (
     <main className="flex-1 flex flex-col items-center justify-center md:px-10 py-2 w-full gap-5">
-      <TituloPanel />
+      <TituloPanel iconTypea={<RxDashboard  />} titulo="Dashboard"/>
       <PanelSuperior />
 
       <div className="grid grid-cols-4 gap-4 w-full">
