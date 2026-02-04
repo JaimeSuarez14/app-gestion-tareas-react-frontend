1d7a491dca7bcf331a3fd6a67b41ba06c627315c
Jaime Suarez <jaisua8@gmail.com>
2026-02-03 19:04:24 -0500

    actualizacion del ruta del home principal

--- src/components/templates/AuthLayout.tsx
+++ src/components/templates/AuthLayout.tsx
@@ -3,7 +3,7 @@ import { Outlet } from "react-router";
 import { SiTask } from "react-icons/si";
 
 const AuthLayout = () => {
-  const rutas = ["login", "register"];
+  const rutas = ["home","login", "register"];
   return (
     <div className="h-screen w-full flex flex-col justify-center items-center text-black px-0 md:px-8">
       <header className=" bg-black w-full p-4">
@@ -21,7 +21,7 @@ const AuthLayout = () => {
                   className={({ isActive }: NavLinkRenderProps) =>
                     `p-2 uppercase rounded-sm border text-amber-50 font-bold hover:bg-green-500/80 border-green-700 ${isActive ? " text-amber-300 " : ""}`
                   }
-                  to={`/${r}`}
+                  to={r==="home" ? "/" : `/${r}`}
                 >
                   {r}
                 </NavLink>
